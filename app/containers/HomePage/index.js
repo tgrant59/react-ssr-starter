8a8c17187778e80f5d170e8fc81ad325af288db6
Tom Grant <t.grant@outlook.com>
2017-07-06 01:01:47 -0400

    Fix errors

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -9,11 +9,10 @@ import styled from 'styled-components'
 import { FormattedMessage } from 'react-intl'
 import messages from './messages'
 
-const Header = styled.h1`margin-to: 0;`
+const Header = styled.h1`margin-top: 0;`
 
 // eslint-disable-next-line react/prefer-stateless-function
 export default class HomePage extends React.PureComponent {
-
   render() {
     return (
       <Header>
