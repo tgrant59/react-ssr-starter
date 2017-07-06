1690b8d1538e79c2dea051f0f7d956c8ad72e3c7
Tom Grant <t.grant@outlook.com>
2017-07-05 23:24:58 -0400

    Test failing tests

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -9,10 +9,11 @@ import styled from 'styled-components'
 import { FormattedMessage } from 'react-intl'
 import messages from './messages'
 
-const Header = styled.h1`margin-top: 0;`
+const Header = styled.h1`margin-to: 0;`
 
 // eslint-disable-next-line react/prefer-stateless-function
 export default class HomePage extends React.PureComponent {
+
   render() {
     return (
       <Header>
